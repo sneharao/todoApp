f28051d5622ddcb85f8ab8ee6051bb98f6be18e2
sneha <sneharao300@gmail.com>
2018-11-04 22:58:00 +0530

    Update src/app/escape.directive.spec.ts

--- src/app/escape.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EscapeDirective } from './escape.directive';
-
-describe('EscapeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EscapeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
